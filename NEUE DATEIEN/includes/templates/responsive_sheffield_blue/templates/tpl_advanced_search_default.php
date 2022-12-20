5b79bb13ae960977654f5f9c0c527d8226230e1c
Jörg Schönberger <office@webchills.at>
2022-12-20 20:56:04 +0100

    Fix for search button and login form fields on mobile

--- NEUE DATEIEN/includes/templates/responsive_sheffield_blue/templates/tpl_advanced_search_default.php
+++ NEUE DATEIEN/includes/templates/responsive_sheffield_blue/templates/tpl_advanced_search_default.php
@@ -2,7 +2,7 @@
 /**
  * Page Template
  *
- * Loaded automatically by index.php?main_page=advanced_search.<br />
+ * Loaded automatically by index.php?main_page=advanced_search
  * Displays options fields upon which a product search will be run
  *
 
@@ -10,7 +10,7 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: tpl_advanced_search_default.php for Sheffield Blue 2022-04-02 15:19:16Z webchills $
+ * @version $Id: tpl_advanced_search_default.php for Sheffield Blue 2022-12-20 20:19:16Z webchills $
  */
 ?>
 <div class="centerColumn" id="advSearchDefault">
